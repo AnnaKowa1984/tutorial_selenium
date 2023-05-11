da4b9a65eaf9a33e25d09599e3f7235757f04fef
Anna Kowalska <annakowa@gmail.com>
2023-05-11 22:49:54 +0200

    lokalizator_id

--- Lokalizator_id.py
+++ Lokalizator_id.py
@@ -7,9 +7,8 @@ from selenium.webdriver.common.by import By
 s = Service(r"ChromeDriverManager().install()")
 driver = webdriver.Chrome(service=s)
 driver.get("file:///C:/Users/Ania/PycharmProjects/zasoby/Test.html")
+sleep(2)
 driver.maximize_window()
-driver.find_element(By.ID, "newPage").click()
-sleep(5)
-# driver.quit()   # zamyka wszystkie okna
-driver.close()  # zamyka tylko to okno, na którym było dziłanie
+sleep(2)
+driver.find_element(By.ID, "fname")
 sleep(10)
