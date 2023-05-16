efc2fdd75fa9aa6361f5684a72f294794bb502eb
Anna Kowalska <annakowa@gmail.com>
2023-05-16 20:26:06 +0200

    wprowadzenie_wartosci_pola_tekstowego

---
+++ wprowadzenie_wartosci_pola_tekstowego.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+s = Service("ChromeDriverManager().install()")
+driver = webdriver.Chrome(service=s)
+driver.get("file:///C:/Users/Ania/PycharmProjects/zasoby/Test.html")
+driver.maximize_window()
+sleep(3)
+driver.find_element(By.ID, "fname").send_keys("Ania")
+sleep(5)
